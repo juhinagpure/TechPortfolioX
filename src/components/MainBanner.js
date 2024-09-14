ce24313e0738827ab890a66df1ad95cca7f65935
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-09-14 09:39:28 +0530

    remove redundant code for cleaner structure

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -4,12 +4,7 @@ import "../styles/MainBanner.css";
 const MainBanner = () => {
   return (
     <section className="main-banner">
-      {/* <div className="banner-text">
-        <h1>
-          Aapka Problem <br />+ Aapka Vision <br />+ Aapka Budget
-        </h1>
-        <h2>= Humara Solution</h2>
-      </div> */}
+    
     </section>
   );
 };
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../styles/Team.css";
-import profileImage from '../assets/images/tej.png'; // Replace with actual image path
+import profileImage from '../assets/images/tej.png'; 
 
 const TeamSection = () => {
   return (
